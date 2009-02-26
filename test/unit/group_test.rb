c32fdc2be4324231c8b5937db147e1ca9157e3ac
Joshua Clingenpeel <joshua.clingenpeel@gmail.com>
2009-02-25 21:11:03 -0800

    adding basic groups tests

--- test/unit/group_test.rb
+++ test/unit/group_test.rb
@@ -5,4 +5,32 @@ class GroupTest < ActiveSupport::TestCase
   test "the truth" do
     assert true
   end
+  
+  test "group has a name" do
+    group = groups(:one)
+    group.name = ""
+    assert ! group.valid?
+    assert group.errors.on(:name)
+  end
+  
+  test "group name is unique" do
+    group = groups(:one)
+    group.name = groups(:two).name
+    assert ! group.valid?
+    assert group.errors.on(:name)
+  end
+  
+  test "group url is valid" do
+    group = groups(:one)
+    group.url = "asdf"
+    assert ! group.valid?
+    assert group.errors.on(:url)
+  end
+  
+  test "group has a reasonably-sized description" do
+    group = groups(:one)
+    group.description = (0..26).map {|i| i.to_s * 72}.to_s
+    assert ! group.valid?
+    assert group.errors.on(:description)
+  end
 end
