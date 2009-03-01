bf953f1905c13abec5aa27be98edc00460ff9cb2
Joshua Clingenpeel <joshua.clingenpeel@gmail.com>
2009-02-28 19:03:48 -0800

    adding the location scaffolding.

---
+++ db/migrate/20090301030225_create_locations.rb
@@ -0,0 +1,17 @@
+class CreateLocations < ActiveRecord::Migration
+  def self.up
+    create_table :locations do |t|
+      t.string :name
+      t.string :address
+      t.float :latitude
+      t.float :longitude
+      t.text :notes
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :locations
+  end
+end
