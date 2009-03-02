e8abb9f117426b8c67afe334c602847bc0425dbe
Joshua Clingenpeel <joshua.clingenpeel@gmail.com>
2009-03-01 17:44:22 -0800

    adding basic tests for event model.

--- test/unit/event_test.rb
+++ test/unit/event_test.rb
@@ -1,8 +1,62 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  test "event has a name" do
+    event = events(:one)
+    event.name = ""
+    
+    assert ! event.valid?
+    assert event.errors.on(:name)
+  end
+  
+  test "event has a description" do
+    event = events(:one)
+    event.description = ""
+    
+    assert ! event.valid?
+    assert event.errors.on(:description)
+  end
+  
+  test "event has a start_date" do
+    event = events(:one)
+    event.start_date = ""
+    
+    assert ! event.valid?
+    assert event.errors.on(:start_date)
+  end
+  
+  test "event has an end_date" do
+    event = events(:one)
+    event.end_date = ""
+    
+    assert ! event.valid?
+    assert event.errors.on(:end_date)
+  end
+  
+  test "event has a group" do
+    event = events(:one)
+    event.group_id = nil
+    
+    assert ! event.valid?
+    assert event.errors.on(:group_id)
+  end
+  
+  test "event has a location" do
+    event = events(:one)
+    event.location_id = nil
+    
+    assert ! event.valid?
+    assert event.errors.on(:location_id)
+  end
+  
+  test "event does not conflict with other event in same place on same day" do
+    event = events(:three)
+    event.location_id = events(:ten).location_id
+    event.start_date = events(:ten).start_date
+    event.end_date = events(:ten).end_date
+    
+    assert ! event.valid?
+    assert event.errors.on(:location)
   end
 end
