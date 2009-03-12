700bd5f36c16907e183f3b9c5b1b29995a3a6182
Joshua Clingenpeel <joshua.clingenpeel@gmail.com>
2009-03-11 18:51:29 -0700

    adding geoip gem to environment.

--- config/environment.rb
+++ config/environment.rb
@@ -30,7 +30,7 @@ Rails::Initializer.run do |config|
   config.gem 'mislav-will_paginate', :version => '~> 2.3.7', :lib => 'will_paginate', 
     :source => 'http://gems.github.com'
   config.gem 'andre-geokit', :version => '~> 1.2.4', :lib => 'geokit'
-
+  config.gem 'geoip'
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
